d1869677103e780e05f499f6b259320a3551f29e
Laphatradap <44722798+Laphatradap@users.noreply.github.com>
2020-02-17 17:30:07 +0100

    add GameRoomContainer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import GameRoomContainer from "./components/Gamerooms";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/gamerooms" component={GameRoomContainer} />
       </BrowserRouter>
       <h1>NaN Game Footer</h1>
     </div>
