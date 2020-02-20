f439cfb07ed2d522962ba04e2624582c222b8f1b
Ramprasad-R <ramprasad.info@gmail.com>
2020-02-20 15:15:27 +0100

    Added redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import reducer from "../reducers";
 import ReduxThunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  blacklist: ["gameRoom", "scoreBoard"],
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
   ? window.__REDUX_DEVTOOLS_EXTENSION__()
   : x => x;
 
 const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
-const store = createStore(reducer, enhancer);
 
-export default store;
+export const store = createStore(persistedReducer, enhancer);
+export const persistor = persistStore(store);
