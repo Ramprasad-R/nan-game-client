652d5e2528dd29a39221e9e702de2523f04f937f
Ramprasad-R <ramprasad.info@gmail.com>
2020-02-21 10:52:25 +0100

    Implemented scoring logic

--- src/components/Gamerooms/Game/GameBoardContainer.jsx
+++ src/components/Gamerooms/Game/GameBoardContainer.jsx
@@ -21,45 +21,38 @@ class GameBoard extends Component {
     guesses: 0,
     matchedCardIndices: [],
     score: 1000,
-    isActive: false
+    isActive: false,
+    gameStarted: false,
+    timer: 0
   };
   timerBoardCompleted = false;
-  guesses1 = 0;
+  guessesCount = 0;
   currentGameRoomId = this.props.location.pathname.split("/").pop();
-  // componentDidMount(){
-  //   console.log(`logging the pathname:`,this.props.history.location.pathname)
-  //   this.props.gameRoomPlayerScore({
-  //     score: this.state.guesses,
-  //     gameroomId: this.props.location.pathname.split("/").pop(),
-  //     userId: this.props.user.id,
 
-  //   })
-  // }
+  interval = null;
 
-  // componentDidUpdate = () => {
-  // const { cards, guesses, matchedCardIndices } = this.state;
-  // const boardCompleted = matchedCardIndices.length === cards.length;
-  // console.log('Board completed')
-  // boardCompleted &&
-  //       this.props.gameRoomPlayerScore({
-  //         score: guesses,
-  //         gameroomId: this.currentGameRoomId,
-  //         userId: this.props.user.id
-  //       })
-  // };
-
-  componentDidUpdate = () => {
-    let interval = null;
-    if (this.state.isActive) {
-      interval = setTimeout(() => {
-        this.setState({
-          score: this.state.score - 10 - this.guesses1
-        });
-      }, 5000);
-    } else if (!this.state.isActive && this.state.score !== 0) {
-      clearInterval(interval);
+  shouldComponentUpdate = (nextProps, nextState) => {
+    console.log(
+      "checking timer",
+      nextState.gameStarted,
+      this.state.gameStarted
+    );
+    if (!nextState.gameStarted) {
+      console.log("GAME FINISSHED!!");
+      clearInterval(this.interval);
     }
-    return () => clearInterval(interval);
+    if (!this.state.gameStarted) {
+      this.interval = setInterval(
+        () =>
+          this.setState({
+            timer: this.state.timer + 1,
+            score: this.state.score - this.state.timer - this.guessesCount
+          }),
+        1000
+      );
+    }
+
+    return nextState.gameStarted;
   };
 
   getFeedbackForCard(index) {
@@ -91,8 +84,12 @@ class GameBoard extends Component {
   // Arrow fx for binding
   handleCardClick = index => {
     const { currentPair } = this.state;
+    if (this.interval && !this.state.gameStarted) {
+      clearInterval(this.interval);
+    }
     this.setState({
-      isActive: true
+      isActive: true,
+      gameStarted: true
     });
 
     if (currentPair.length === 2) {
@@ -111,17 +108,12 @@ class GameBoard extends Component {
 
     const newPair = [currentPair[0], index];
     const newGuesses = guesses + 1;
-    this.guesses1 = newGuesses;
+    this.guessesCount = newGuesses;
     const matched = cards[newPair[0]] === cards[newPair[1]];
     this.setState({
       currentPair: newPair,
       guesses: newGuesses
     });
-    if (this.timerBoardCompleted && this.state.score <= 0) {
-      this.setState({
-        isActive: !this.state.isActive
-      });
-    }
 
     if (matched) {
       this.setState(
@@ -134,31 +126,24 @@ class GameBoard extends Component {
           this.timerBoardCompleted = boardCompleted;
           console.log("Board completed", boardCompleted);
           if (boardCompleted) {
+            this.setState({
+              isActive: !this.state.isActive,
+              gameStarted: !this.state.gameStarted
+            });
             this.props.gameRoomPlayerScore({
-              score: guesses,
+              score: this.state.score,
               gameroomId: this.currentGameRoomId,
               userId: this.props.user.id
             });
           }
         }
       );
-
-      // const boardCompleted = matchedCardIndices.length === cards.length;
-      // console.log('Board completed')
-      // if(boardCompleted){
-      //       this.props.gameRoomPlayerScore({
-      //         score: guesses,
-      //         gameroomId: this.currentGameRoomId,
-      //         userId: this.props.user.id
-      //       })
-      //     }
     }
     setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS);
   };
 
   render() {
     const { cards, guesses } = this.state; //matchedCardIndices
-    // const boardCompleted = matchedCardIndices.length === cards.length;
     console.log(`logging the pathname:`, this.props.history.location.pathname);
     console.log(this.state, this.props);
     return (
@@ -166,8 +151,10 @@ class GameBoard extends Component {
         <Timer
           boardcompleted={this.timerBoardCompleted}
           score={this.state.score}
+          timer={this.state.timer}
+          guesses={guesses}
         />
-        <GuessCount guesses={guesses} />
+        {/* <GuessCount guesses={guesses} /> */}
         {cards.map((card, index) => (
           <Card
             card={card}
@@ -177,14 +164,7 @@ class GameBoard extends Component {
             onClick={this.handleCardClick}
           />
         ))}
-        {/* {won && <HallOfFame entries={FAKE_HOF} />} */}
-        {/* {boardCompleted &&
-          this.props.gameRoomPlayerScore({
-            score: guesses,
-            gameroomId: this.currentGameRoomId,             //this snippet is moved up to handleNewPairClosedBy fx(line88) where called as callback!
-            userId: this.props.user.id
-          })} */}
-        {/* {won} */}
+
         <ScoreBoard gameroomId={this.currentGameRoomId} />
         <Link to="/gamerooms" style={{ color: "pink" }}>
           <p>Back to Gameroom</p>
@@ -195,7 +175,7 @@ class GameBoard extends Component {
 }
 
 const mapStateToProps = state => {
-  console.log("state of GR", state);
+  //console.log("state of GR", state);
   return {
     rooms: state.gameRoom,
     user: state.user.user
